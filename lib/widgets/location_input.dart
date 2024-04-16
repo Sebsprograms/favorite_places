d09e6a049848d1bfc1ede77121c70e9e4545b7f4
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-04-16 12:01:37 -0600

    Preview location using google static maps api

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -9,8 +14,16 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _selectedLocation;
+  PlaceLocation? _selectedLocation;
   bool _isGettingLocation = false;
+
+  String get locationImageUrl {
+    if (_selectedLocation == null) return '';
+
+    final lat = _selectedLocation!.latitude;
+    final lng = _selectedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=${dotenv.env['GOOGLE_API_KEY']}';
+  }
 
   void _getCurrentLocation() async {
     Location location = new Location();
@@ -39,8 +52,26 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=${dotenv.env['GOOGLE_API_KEY']}');
+
+    final response = await http.get(url);
+    final responseJson = json.decode(response.body);
+    final address = responseJson['results'][0]['formatted_address'];
 
     setState(() {
+      _selectedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
       _isGettingLocation = false;
     });
   }
@@ -55,6 +86,14 @@ class _LocationInputState extends State<LocationInput> {
           ),
     );
 
+    if (_selectedLocation != null) {
+      previewContent = Image.network(
+        locationImageUrl,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
     }
